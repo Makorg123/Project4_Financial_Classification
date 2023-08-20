d2c443e8b57291db0afc1ff12641f47aef447348
Anas Khan <84630559+Makorg123@users.noreply.github.com>
2023-08-20 19:43:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ st.markdown('This app predicts the :blue[financial risk] of a person based on it
 st.header('User Input Parameters')
 col1, col2, col3 = st.columns(3)
 with col1:
-   city = st.number_input('City', min_value=0, max_value=44, value=1)
-   Location_Score = st.number_input('Location_Score', min_value=0.00, max_value=100.00, value=10.00)
+   city = st.number_input('City Code from [0-44]', min_value=0, max_value=44, value=1)
+   Location_Score = st.number_input('Location_Score from [0-100]', min_value=0.00, max_value=100.00, value=10.00)
 with col2:
-   Internal_Audit_Score = st.number_input('Internal_Audit_Score', min_value=0, max_value=15, value=1)
-   External_Audit_Score = st.number_input('External_Audit_Score', min_value=0, max_value=15, value=1)
+   Internal_Audit_Score = st.number_input('Internal_Audit_Score from [0-15]', min_value=0, max_value=15, value=1)
+   External_Audit_Score = st.number_input('External_Audit_Score from [0-15]', min_value=0, max_value=15, value=1)
 with col3:
-   Fin_Score = st.number_input('Fin_Score', min_value=0, max_value=15, value=1)
-   Loss_score = st.number_input('Loss_score', min_value=0, max_value=13, value=1)
+   Fin_Score = st.number_input('Fin_Score from [0-15]', min_value=0, max_value=15, value=1)
+   Loss_score = st.number_input('Loss_score from [0-13]', min_value=0, max_value=13, value=1)
    Past_Results = st.slider('Past_Results', 0,10,1)
 
 if st.button('Risk Prediction'):
