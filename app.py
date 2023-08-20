10fb3aa6e08d610abc3322deba5ccce5cdfa2f5a
Makorg123 <132anaskhan@gmail.com>
2023-08-20 16:35:31 +0530

    app.py is created

---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+st.title('Financial Risk Prediction')
+st.markdown('This app predicts the :blue[financial risk] of a person based on its financial statements.')
+
+st.header('User Input Parameters')
+col1, col2, col3 = st.columns(3)
+with col1:
+   city = st.number_input('City', min_value=0, max_value=44, value=1)
+   Location_Score = st.number_input('Location_Score', min_value=0.00, max_value=100.00, value=10.00)
+with col2:
+   Internal_Audit_Score = st.number_input('Internal_Audit_Score', min_value=0, max_value=15, value=1)
+   External_Audit_Score = st.number_input('External_Audit_Score', min_value=0, max_value=15, value=1)
+with col3:
+   Fin_Score = st.number_input('Fin_Score', min_value=0, max_value=15, value=1)
+   Loss_score = st.number_input('Loss_score', min_value=0, max_value=13, value=1)
+   Past_Results = st.slider('Past_Results', 0,10,1)
+
+if st.button('Risk Prediction'):
+   st.header('Risk Prediction')
+   # Read the dataset
+   df = pd.read_csv('Train.csv')
+   df = df.drop(['IsUnderRisk'],axis=1)
+   # Load the model
+   import pickle
+   model = pickle.load(open('random_forest_model.pkl', 'rb'))
+   # Apply model to make predictions
+   prediction = model.predict([[city,Location_Score,Internal_Audit_Score,External_Audit_Score,Fin_Score,Loss_score,Past_Results]])
+   if prediction == 0:
+      st.markdown('The financial risk of the person is :green[Low]')
+   else:
+      st.markdown('The financial risk of the person is :red[High]')
